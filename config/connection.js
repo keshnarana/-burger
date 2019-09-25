fd0507cc2168c37140851793c2aa78cd183311d2
kesh24 <keshnarana4@gmail.com>
2019-09-25 16:59:16 -0400

    done

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Connect Node to MySQL.
+var mysql = require("mysql");
+
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Nimesh1!",
+        database: "burgers_db"
+    });
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection.
+module.exports = connection;
