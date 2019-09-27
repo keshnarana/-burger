04fe708a8cad87c608e4b25da23183f4910f357e
kesh24 <keshnarana4@gmail.com>
2019-09-27 19:22:44 -0400

    finaal

--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,27 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
+if (process.env.CLEARDB_DATABASE_URL) {
+	
+	connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'Nimesh1!',
+		database: 'burgers_db'
+    })
+};
 
-var db_config  = mysql.createConnection({
-        host: "us-cdbr-iron-east-02.cleardb.net",
-        user: "bf48eacefdf541",
-        password: "7042638b",
-        database: "heroku_b7067b0921c5731"
-    });
 
-
-
-var connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err.stack);
-      
-      setTimeout(handleDisconnect, 2000);
-      return; // We introduce a delay before attempting to reconnect,
-    }     
-    console.log("connected as id " + connection.threadId);          // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
     }
-  });
-}
+    console.log("connected as id " + connection.threadId);
+});
 
-handleDisconnect();
 // Export the connection.
 module.exports = connection;
